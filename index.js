7bb9895ef94599a26dbd2facdd9def8a99136673
Victor <victor.chumba@student.moringaschool.com>
2023-03-21 13:53:52 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+function divide(x,y){
+    let c = 2000
+    let k = 100
+    return Math.floor(c/k);
+
+}
+divide();
+
+
+function square(x) {
+    return x * x;
+}
+  console.log(2)
+
+
+const add = (a , b) => {
+    return a + b
+}
+  console,log(add(3,4))
+
+
